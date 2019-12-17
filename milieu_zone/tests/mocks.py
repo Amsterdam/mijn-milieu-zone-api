5a67812af52185b5881443b6698e8e56b39bb026
Johan Otten <drakonen@drakonen.com>
2019-12-17 15:11:55 +0100

    Moved to correct file, split off mocks

---
+++ milieu_zone/tests/mocks.py
@@ -0,0 +1,11 @@
+class ResponseMock:
+    def content(self):
+        return ''
+
+
+class RequestsMock:
+    def post(self, *args, **kwargs):
+        print("post", args, kwargs)
+        return ResponseMock()
+
+
